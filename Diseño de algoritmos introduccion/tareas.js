cd398299a5efd5ca886a8b90c99082377c89f91e
Daniel Calamateo <dany.calamateo@gmail.com>
2021-12-19 21:39:53 -0600

    first commit

---
+++ Diseño de algoritmos introduccion/tareas.js
@@ -0,0 +1,17 @@
+//Se crea la variable para ingresar tareas. ParseInt es para convertir cadenas en enteros
+let tareas=parseInt(prompt("¿Cuantas tareas entregaste?"));
+
+if (tareas>=0&&tareas<=6){
+    alert("Failed");
+}
+else if (tareas>=7&&tareas<9) {
+    alert("Insufficient");
+} 
+else if (tareas>=9&&tareas<=14) {
+    alert("Good");
+} 
+else if (tareas==15) {
+    alert("Excellent");
+} 
+else {
+alert("Error, dato no valido"); }
